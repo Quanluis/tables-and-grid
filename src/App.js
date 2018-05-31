910d9afd9d1ceb5d37f7514acf9ebe91058e17ce
Luis Garzon <luisgarzon90@yahoo.com>
2018-05-31 11:47:35 -0400

    Created a simple grid layout for websites, need to add additional styling with CSS.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
 import React, { Component } from 'react';
+import './Index.css'
 
 class App extends Component {
   render() {
     return (
      <div>
-      <h1>
-        <p>Grid and Tables</p>
-      </h1>
+      <table>
+        <caption>Employee information</caption>
+        <tfoot>This is a table footer</tfoot>
+        <thead>
+        <tr> 
+          <th scope = "col" >Luis Garzon</th>
+          <th scope = "col">Web developer</th>
+          <th scope = "col">quanluis@protonmail.com</th>
+        </tr>
+        </thead>
+        <tr> 
+          <th scope = "row" >Luis Garzon</th>
+          <th>Web developer</th>
+          <th>quanluis@protonmail.com</th>
+        </tr>
+      </table>
+      <div>
+        <div className = "grid">    
+        <header>Header</header>
+        <nav>Nav</nav>
+        <main>Main</main>
+        <aside>Aside</aside>
+        <footer>Footer</footer>
+        </div>
+      </div>
      </div>    
     );
   }
